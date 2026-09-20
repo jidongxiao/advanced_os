612c03a5fd514a1df22dc460d083cee484daa433
Jidong Xiao <jidong.xiao@gmail.com>
2026-09-20 02:47:00 -0400

    adding the shared header

---
+++ labs/cache/shared.h
@@ -0,0 +1,11 @@
+#ifndef SHARED_H
+#define SHARED_H
+
+#include <stdint.h>
+
+#define SHM_NAME "/cache_lab_shm"
+#define ARRAY_SIZE 256
+#define STRIDE 64
+#define SHM_SIZE (ARRAY_SIZE * STRIDE)
+
+#endif
